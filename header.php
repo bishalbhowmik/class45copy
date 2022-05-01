085b7344f1d27d6c8aec476237adc9dfa982069f
Bishal <bishal1831047@gmail.com>
2022-05-01 18:21:03 +0600

    third

---
+++ header.php
@@ -0,0 +1,29 @@
+<?php 
+
+$title_name= "HOMEPAGE";
+include('function.php');
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php title(); ?></title>
+</head>
+<body>
+    <header class="header-area">
+        <nav class="main-menu">
+            <ui>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOME</a></li>
+            </ui>
+        </nav>
+    </header>
